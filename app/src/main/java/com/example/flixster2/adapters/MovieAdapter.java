fabaa8ccc8852befb085da43d09f71a3d5af125b
AFitting <asf45@humboldt.edu>
2020-02-04 18:48:00 -0800

    just saving

--- app/src/main/java/com/example/flixster2/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster2/adapters/MovieAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixster2.R;
 import com.example.flixster2.models.Movie;
 
@@ -64,6 +65,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
         }
     }
 }
